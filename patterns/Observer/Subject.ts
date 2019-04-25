f4519a83ff5fdd987465ec6f2784eb24c1b3885d
Vincent Battez <vincent.battez.pro@gmail.com>
2019-04-25 21:14:58 +0200

    docs(observer): add readme

--- patterns/Observer/Subject.ts
+++ patterns/Observer/Subject.ts
@@ -1,4 +1,4 @@
-import { ObserverInterface } from './ObserverInterface'
+import { ObserverInterface } from './Interfaces/ObserverInterface'
 
 class Subject {
   private observers: ObserverInterface[];
@@ -7,19 +7,19 @@ class Subject {
     this.observers = []
   }
 
-  attach (observerName:string, callback:() => void) {
+  public attach (observerName:string, callback:() => void):void {
     this.observers.push({
         name: observerName,
         callback
       });
   }
 
-  detach (observerName:string) {
+  public detach (observerName:string):void {
     this.observers = this.observers
       .filter(observer => observer.name !== observerName);
   }
 
-  notify (observerName:string) {
+  public notify (observerName:string):void {
     this.observers
       .filter(observer => observer.name === observerName)
       .map(o => o.callback());
---
+++ patterns/Observer/readme.md
@@ -0,0 +1,5 @@
+# Observer
+**Description:** https://sourcemaking.com/design_patterns/observer
+
+### Structure
+![Strcuture Observer](https://www.tutorialspoint.com/design_pattern/images/observer_pattern_uml_diagram.jpg)
